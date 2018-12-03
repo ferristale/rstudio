02fd036c2ae51c77be184ed0319be78a90fdfaf0
dengyh1994 <dengyh1994@yeah.net>
2018-12-03 10:34:35 +0800

    我才没问题！

--- read.R
+++ read.R
@@ -6,10 +6,10 @@ for (i in 1:12) {
   assign(paste('gas.zq', '13', 01:12,sep = '.')[i] , read.csv(name[i]))
 }
 
-#yrdy
 name <- paste( 'data/', dir('data')[1:12],sep = '')
 gas.zq4 <- read.csv(name[1])
 for (i in 2:length(name)) {
   tmp <- read.csv(name[i])
   gas.zq4 <- merge(gas.zq4, tmp, all=TRUE)
 }
+
